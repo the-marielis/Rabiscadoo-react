7128b447fdba0fc512e98c1c598468a64f19df85
Marieli Teixeira <142545847+the-marielis@users.noreply.github.com>
2025-05-21 21:20:04 -0300

    ajuste no layout da tela de home logado

--- src/components/HomeLogado.jsx
+++ src/components/HomeLogado.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import '../css/main.css';
+import { Link } from "react-router-dom";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -10,7 +11,7 @@ import tattoo3 from "../../public/images/tatuagem3.jpg";
 import tattoo4 from "../../public/images/tatuagem4.jpg";
 import tattoo5 from "../../public/images/tatuagem5.jpg";
 import tattoo6 from "../../public/images/tatuagem6.jpg";
-import manchaImg from "../../public/images/mancha1.png";
+import manchaImg from "../../public/images/mancha3.png";
 const HomeLogado = () => {
     const settings = {
         dots: true,
@@ -24,13 +25,16 @@ const HomeLogado = () => {
     
       return (
         <div className="container-home">
+          <div className="txt-home">
+            <h2>Não perca tempo! <br />
+        Conheça hoje nosso time</h2>
+          </div>
           <div className="slider">
             <Slider {...settings}>
               <div><img src={tattoo1} alt="Tattoo 1" /></div>
               <div><img src={tattoo2} alt="Tattoo 2" /></div>
               <div><img src={tattoo3} alt="Tattoo 3" /></div>
               <div className="tattoo4"><img src={tattoo4} alt="Tattoo 4" /></div>
-              <div><img src={tattoo5} alt="Tattoo 5" /></div>
               <div><img src={tattoo5} alt="Tattoo 5" /></div>
               <div><img src={tattoo6} alt="Tattoo 6" /></div>
             </Slider>
@@ -39,11 +43,7 @@ const HomeLogado = () => {
           <div className="img-fundo">
             <img src={manchaImg} alt="mancha" />
           </div>
-          <div className="entrar">
-            <button>
-              <a href="#">VENHA <br /> CONHECER</a>
-            </button>
-          </div>
+          <Link to="/profissionais" className="btn-entrar"> AGENDE <br /> JÁ</Link>
         </div>
       );
 };
