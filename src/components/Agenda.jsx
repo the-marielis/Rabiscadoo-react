332545a13eccd210db3d1b426476857db6be2f35
Marieli Teixeira <142545847+the-marielis@users.noreply.github.com>
2025-05-21 22:17:43 -0300

    criação da tela de agenda

--- src/components/Agenda.jsx
+++ src/components/Agenda.jsx
@@ -1,12 +1,61 @@
 import React from "react";
-import '../css/main.css';
+import '../css/agenda.css';
 
 const Agenda = () => {
-    return (
-        <div>
-            <h1>Agenda</h1>
-        </div>
-    );
+  return (
+    <section className="secao-agenda">
+      <div className="form-container">
+        <h2>Fala mais do seu projeto pra nós</h2>
+
+        <form>
+          <div className="linha">
+            <label>
+              Profissional:
+              <input type="text" value="Fulana da Silva" disabled />
+            </label>
+
+            <label>
+              Estilo da tattoo:
+              <select>
+                <option value="">selecione</option>
+                {/* Adicione mais opções aqui */}
+              </select>
+            </label>
+          </div>
+
+          <div className="linha">
+            <label>
+              Tamanho:
+              <input type="text" placeholder="cm" />
+            </label>
+
+            <label>
+              Local do corpo:
+              <input type="text" placeholder="escreve aqui" />
+            </label>
+
+            <label>
+              Tattoo colorida?
+              <select>
+                <option value="">selecione</option>
+              </select>
+            </label>
+          </div>
+
+          <label>
+            Qual sua ideia para esse projeto?
+            <textarea placeholder="Descreva sua ideia aqui..."></textarea>
+          </label>
+
+          <div className="linha">
+            <input type="file" />
+          </div>
+
+          <button type="submit">confirmar</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Agenda;
