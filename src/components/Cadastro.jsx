8472905ce71ae0258df8361d4b8298c48bb6cde8
Marieli Teixeira <142545847+the-marielis@users.noreply.github.com>
2025-05-22 12:58:15 -0300

    transformando o form do cadastro em elemento de componente

--- src/components/Cadastro.jsx
+++ src/components/Cadastro.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import '../css/cadastro.css'; 
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import '../css/cadastro.css';
+import { Link, useNavigate } from 'react-router-dom';
+import CamposCadastro from '../components/CamposCadastro.jsx';
 
 const CadastroForm = () => {
   const [formData, setFormData] = useState({
@@ -22,33 +21,17 @@ const CadastroForm = () => {
     tp_cadastro: '',
   });
 
+  const [mensagemErro, setMensagemErro] = useState('');
   const Navigate = useNavigate();
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-const [mensagemErro, setMensagemErro] = useState('');
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  setMensagemErro('');
-
-  try {
-    const response = await axios.post('http://localhost:3301/api/cadastro', formData);
-    console.log(response.data);
-    alert('Cadastro realizado com sucesso!');
-    return Navigate("/login");
-  } catch (error) {
-    console.error('Erro no cadastro:', error);
-    if (error.response && error.response.data && error.response.data.error) {
-      setMensagemErro(error.response.data.error); // mensagem vinda do backend
-    } else {
-      setMensagemErro('Erro ao cadastrar. Tente novamente.');
-    }
-  }
-};
-
+  const handleInput = (e) => {
+    e.target.value = e.target.value.replace(/\D/g, '');
+  };
 
   const handleDateFocus = (e) => {
     e.target.type = 'date';
@@ -60,8 +43,23 @@ const handleSubmit = async (e) => {
     }
   };
 
-  const handleInput = (e) => {
-    e.target.value = e.target.value.replace(/\D/g, '');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMensagemErro('');
+
+    try {
+      const response = await axios.post('http://localhost:3301/api/cadastro', formData);
+      console.log(response.data);
+      alert('Cadastro realizado com sucesso!');
+      return Navigate("/login");
+    } catch (error) {
+      console.error('Erro no cadastro:', error);
+      if (error.response && error.response.data && error.response.data.error) {
+        setMensagemErro(error.response.data.error);
+      } else {
+        setMensagemErro('Erro ao cadastrar. Tente novamente.');
+      }
+    }
   };
 
   return (
@@ -81,147 +79,22 @@ const handleSubmit = async (e) => {
               </div>
               <div className="login-button">
                 <button style={{ marginRight: '50px' }}>
-                  <Link to="/login"> Entrar</Link>
+                  <Link to="/login">Entrar</Link>
                 </button>
               </div>
             </div>
-            <div className="input-group">
-              <div className="line1">
-                <input type="text" 
-                id="nome" 
-                name="nome" 
-                placeholder="Nome Completo" 
-                required 
-                maxLength="900" 
-                onChange={handleChange} />
-              </div>
-              <div className="line2">
-                <input
-                  type="text"
-                  id="nascimento"
-                  name="nascimento"
-                  placeholder="Nascimento"
-                  onFocus={handleDateFocus}
-                  onBlur={handleDateBlur}
-                  onChange={handleChange}
-                />
-                <input
-                  type="text"
-                  id="CPF"
-                  name="CPF"
-                  placeholder="CPF"
-                  required
-                  onInput={handleInput}
-                  maxLength="9"
-                  style={{ marginLeft: '1%' }}
-                  onChange={handleChange}
-                />
-                <input
-                  type="text"
-                  id="RG"
-                  name="RG"
-                  placeholder="RG"
-                  onInput={handleInput}
-                  maxLength="9"
-                  style={{ marginLeft: '1%' }}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="line3">
-                <input type="text" 
-                id="cidade" 
-                name="cidade" 
-                placeholder="Qual sua cidade?" 
-                required  
-                onChange={handleChange}/>
-              </div>
-              <div className="line4">
-                <input
-                  type="text"
-                  id="CEP"
-                  name="CEP"
-                  placeholder="CEP"
-                  required
-                  onInput={handleInput}
-                  maxLength="8"
-                  onChange={handleChange}
-                />
-                <input type="text" 
-                id="endereco" 
-                name="endereco" 
-                placeholder="Endereço completo" 
-                required 
-                onChange={handleChange} />
 
-              </div>
-              <div className="line5">
-                <input
-                  type="text"
-                  id="telefone1"
-                  name="telefone"
-                  placeholder="Telefone principal"
-                  required
-                  onInput={handleInput}
-                  maxLength="11"
-                  onChange={handleChange}
-                />
-                <input
-                  type="tel"
-                  id="telefone2"
-                  name="telefone2"
-                  placeholder="Telefone para recado"
-                  onInput={handleInput}
-                  maxLength="11"
-                  style={{ marginLeft: '1%' }}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="line6">
-                <input type="email" id="email" name="email" placeholder="Digite seu e-mail"  required onChange={handleChange}/>
-                <input
-                  type="text"
-                  id="user"
-                  name="nome_usuario"
-                  placeholder="Nome de usuário"
-                  required
-                  style={{ marginLeft: '1%' }}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="line7">
-                <input type="password" id="password" name="senha" placeholder="Digite sua senha" required onChange={handleChange}/>
-                <input
-                  type="password"
-                  id="confirme-senha"
-                  name="confirme-senha"
-                  placeholder="Confirme sua senha"
-                  required
-                  style={{ marginLeft: '1%' }}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="tp-cadastro">
-                <div className="tipo">
-                  <label htmlFor="tp_cadastro"><b>Você é:</b></label>
-                </div>
-                <div className="option">
-                  <input type="radio" id="tp-rabiscadoo" 
-                  name="tp_cadastro" value="rabiscadoo" 
-                  onChange={handleChange} />{' '}
-                  <label htmlFor="tp-rabiscadoo">Rabiscadoo</label>
-                  <br />
+            <CamposCadastro
+              formData={formData}
+              handleChange={handleChange}
+              handleDateFocus={handleDateFocus}
+              handleDateBlur={handleDateBlur}
+              handleInput={handleInput}
+            />
 
-                  <input type="radio"
-                  id="tp-tatu" name="tp_cadastro" 
-                  value="tatuador" 
-                  onChange={handleChange} />{' '}
-                  <label htmlFor="tp-tatu">Tatuador</label>
-                </div>
-              </div>
-            </div>
             <div className="continue-button">
               {mensagemErro && (
-              <p style={{ color: 'red', marginBottom: '10px' }}>{mensagemErro}</p>
+                <p style={{ color: 'red', marginBottom: '10px' }}>{mensagemErro}</p>
               )}
               <button type="submit">bora lá!</button>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './components/Login.jsx';
 import Cadastro from './components/Cadastro.jsx';
+import CadastroForm from './components/CamposCadastro.jsx';
 import Home from './components/Home.jsx';
 import Sobre from './components/Sobre.jsx';
 import Perfil from './components/Perfil.jsx';
