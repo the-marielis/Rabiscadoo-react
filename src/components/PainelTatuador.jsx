50e669c5eafee80f00ea471854ef890e02abbbe4
Marieli Teixeira <142545847+the-marielis@users.noreply.github.com>
2025-06-16 13:24:13 -0300

    gambiarraaaaa

--- src/components/PainelTatuador.jsx
+++ src/components/PainelTatuador.jsx
@@ -1,21 +1,20 @@
 //import React, {useState} from "react";
-import {GoGear, GoHistory, GoPencil} from "react-icons/go";
+import { GoGear, GoHistory, GoPencil } from "react-icons/go";
 import "../css/perfilUsuario.css";
 import BotaoContinuar from "./BotaoContinuar";
-import {useAuth} from "../context/AuthContext";
+import { useAuth } from "../context/AuthContext";
 import HistoricoList from "../components/HistoricoList/HistoricoList";
 import PortfolioCarousel from "../components/Carousel/PortfolioCarousel";
 import PortfolioEditor from "../components/PortfolioEditor/PortfolioEditor.jsx";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Toast from "./Toast/Toast.jsx";
-import React, {useEffect, useRef, useState} from "react";
-import {converteDataUsa} from "../Utils/converteData.js";
+import React, { useEffect, useRef, useState } from "react";
+import { converteDataUsa } from "../Utils/converteData.js";
 import BotaoDeletarConta from "../components/BotaoDeletarConta/BotaoDeletarConta.jsx";
-
 
 const HomeLogado = () => {
   const navigate = useNavigate();
-  const [toast, setToast]  = useState(null);
+  const [toast, setToast] = useState(null);
   const { usuario } = useAuth();
   const [editando, setEditando] = useState(false);
   const [configurando, setConfigurando] = useState(false);
@@ -25,22 +24,21 @@ const HomeLogado = () => {
   const [preview, setPreview] = useState(null);
 
   const [formData, setFormData] = useState({
-    idususario:"",
+    idususario: "",
     nome: "",
     email: "",
     telefone: "",
     nascimento: "",
     avatar: null,
-    senha:"",
+    senha: "",
     tp_cadastro: "",
     editouConfig: false,
     idTatuador: "",
     descricao: "",
     imagem: "",
     instagram: "",
-    portifolio_url:"",
+    portifolio_url: "",
   });
-
 
   // Atualiza formData quando usuario for carregado
   useEffect(() => {
@@ -55,8 +53,8 @@ const HomeLogado = () => {
         senha: usuario.senha || "",
         tp_cadastro: usuario.tp_cadastro || "",
         editouConfig: false,
-        idTatuador:usuario.idTatuador || "",
-        descricao:usuario.descricao || "",
+        idTatuador: usuario.idTatuador || "",
+        descricao: usuario.descricao || "",
         imagem: usuario.imagem || "",
         instagram: usuario.instagram || "",
         portifolio_url: usuario.portifolio_url || "",
@@ -67,9 +65,6 @@ const HomeLogado = () => {
   const showToast = (message, type = "error") => setToast({ message, type });
   const toggleEdicao = () => setEditando(!editando);
   const toggleConfiguracao = () => setConfigurando(!configurando);
-
-
-
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -119,121 +114,127 @@ const HomeLogado = () => {
   };
 
   return (
-      <>
-        {toast && (
-            <div className="toast-container">
-              <Toast message={toast.message} type={toast.type} onClose={() => setToast(null)} />
-            </div>
-        )}
+    <>
+      {toast && (
+        <div className="toast-container">
+          <Toast
+            message={toast.message}
+            type={toast.type}
+            onClose={() => setToast(null)}
+          />
+        </div>
+      )}
 
-    <div
-      className="perfil-container"
-      style={{ backgroundImage: "url('/images/BACKGROUND_LOGIN.png')" }}
-    >
-      <section className="perfil-card">
-        <h1 className="perfil-title">Seu espaço</h1>
+      <div
+        className="perfil-container"
+        style={{ backgroundImage: "url('/images/BACKGROUND_LOGIN.png')" }}
+      >
+        <section className="perfil-card">
+          <h1 className="perfil-title">Seu espaço</h1>
 
-        <div className="perfil-grid">
-          <article className="perfil-box info-principal">
-            <div className="avatar-tatu"
-                 onClick={handleIconClick}
-                 style={{
-                   cursor: "pointer",
-                   position: "relative",
-                   overflow: "hidden",
-                 }}>
-
-              {preview || formData.imagem ? (
+          <div className="perfil-grid">
+            <article className="perfil-box info-principal">
+              <div
+                className="avatar-tatu"
+                onClick={handleIconClick}
+                style={{
+                  cursor: "pointer",
+                  position: "relative",
+                  overflow: "hidden",
+                }}
+              >
+                {preview || formData.imagem ? (
                   <img
-                      src={
-                          preview ||
-                          (formData.imagem
-                              // ? `http://localhost:3301/${formData.imagem}`
-                              ? `http://localhost:5173/images/fulana.png`
-                              : "/images/default-avatar.png")
-                      }
-                      alt=""
-                      className="avatar-img"
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                        borderRadius: "50%",
-                      }}
+                    src={
+                      preview ||
+                      (formData.imagem
+                        ? // ? `http://localhost:3301/${formData.imagem}`
+                          `http://localhost:5173/images/fulana.png`
+                        : "/images/default-avatar.png")
+                    }
+                    alt=""
+                    className="avatar-img"
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                      borderRadius: "50%",
+                    }}
                   />
-              ) : null}
+                ) : null}
 
-              <GoPencil className="avatar-tatu-pencil" />
-              <input
+                <GoPencil className="avatar-tatu-pencil" />
+                <input
                   type="file"
                   ref={inputFileRef}
                   style={{ display: "none" }}
                   accept="image/*"
                   onChange={handleArquivoChange}
-              />
-            </div>
-
-            {/* Ícone/avatar + dados */}
-
-            <div className="dados">
-              <div className="linha-nome">
-                <h2>{usuario?.nome || "Nome do Tatuador"}</h2>
-                <GoPencil />
+                />
               </div>
-              <p>{usuario?.email || "email@email.com"}</p>
-              <p>Telefone: {usuario?.telefone || "Não informado"}</p>
-              <p>Data de nascimento: {usuario?.nascimento || "00/00/0000"}</p>
-              <div className="linha-informacoes">
-                <div className="linha-historicos">
-                  <h3>Históricos</h3>
-                  <GoHistory />
+
+              {/* Ícone/avatar + dados */}
+
+              <div className="dados">
+                <div className="linha-nome">
+                  <h2>{usuario?.nome || "Nome do Tatuador"}</h2>
+                  <GoPencil />
                 </div>
-                <HistoricoList papel="tatuador" scope="todos" />
-                <div className="linha-privacidade">
-                  <h3>Privacidade e Segurança</h3>
-                  <GoGear />
-                </div>
-                <p>Alterar senha</p>
-                <p>Mudar para perfil profissional</p>
-                <p>Alterar preferências da conta</p>
-                <p>Ocultar pessoas</p>
-                <BotaoDeletarConta
+                <p>{usuario?.email || "email@email.com"}</p>
+                <p>Telefone: {usuario?.telefone || "Não informado"}</p>
+                <p>Data de nascimento: {usuario?.nascimento || "00/00/0000"}</p>
+                <div className="linha-informacoes">
+                  <div className="linha-historicos">
+                    <h3>Históricos</h3>
+                    <GoHistory />
+                  </div>
+                  <HistoricoList papel="tatuador" scope="todos" />
+                  <div className="linha-privacidade">
+                    <h3>Privacidade e Segurança</h3>
+                    <GoGear />
+                  </div>
+                  <p>Alterar senha</p>
+                  <p>Mudar para perfil profissional</p>
+                  <p>Alterar preferências da conta</p>
+                  <p>Ocultar pessoas</p>
+                  <BotaoDeletarConta
                     idusuario={usuario?.idusuario}
                     idTatuador={usuario?.idTatuador}
                     showToast={showToast}
-                />
+                  />
+                </div>
               </div>
-            </div>
-          </article>
+            </article>
 
-          <article className="perfil-box agendamentos">
-            <div className="linha-agendas">
-              <h3>Próximos Agendamentos</h3>
-            </div>
-            <div>
-            <HistoricoList papel="tatuador" scope="proximos" />
-            </div>
-<div className="portfolio">
-  <div className="linha-portfolio">
-    <h3>Portfólio</h3>
-    <GoPencil />
-  </div>
+            <article className="perfil-box agendamentos">
+              <div className="linha-agendas">
+                <h3>Próximos Agendamentos</h3>
+              </div>
+              <div>
+                <HistoricoList papel="tatuador" scope="proximos" />
+              </div>
+              <div className="portfolio">
+                <div className="linha-portfolio">
+                  <h3>Portfólio</h3>
+                  <GoPencil />
+                </div>
 
-  <PortfolioCarousel
-    idusuario={usuario?.idusuario}
-    imagensPorPagina={3}
-    modoCompacto={true}
-  />
-  <PortfolioEditor idusuario={usuario?.idusuario} />
-</div>
-
-          </article>
-          <br />
-        </div>
-        <BotaoContinuar texto="confirmar" largura="35%" />
-      </section>
-    </div>
-   </>
+                <PortfolioCarousel
+                  idusuario={usuario?.idusuario}
+                  imagensPorPagina={3}
+                  modoCompacto={true}
+                />
+                <PortfolioEditor idusuario={usuario?.idusuario} />
+              </div> <br /><br /><br /><br />
+              <span className="span-continuar">
+              <BotaoContinuar texto="confirmar" largura="35%" />
+              </span>
+            </article>
+            
+          </div>
+        </section>
+      </div>
+    </>
   );
 };
 
